53eb07de92f12a2c991de7154de12cd1103d4863
Aidana Rezhabova <rezhabovaaidana.gmail.com>
2023-02-06 12:08:17 +0600

    kvf is created

--- code.py
+++ code.py
@@ -2,3 +2,9 @@
 print("Hello world!")
 def title():
     print("Title")
+def kvf(a):
+    return a*a;
+
+a=12
+res=kvf(a)
+print('a=', a)
--- demo.java
+++ demo.java
@@ -1,5 +1,12 @@
 public class Demo{
     public static void main(String[] args) {
         System.out.println("Helloooo")
+        int a=12;
+        int res=kvf(a);
+        System.out.print(res);
+
+    }
+    public static int kvf(int a){
+    return a*a
     }
 }
