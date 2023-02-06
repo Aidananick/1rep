77c93e2fb8590bac6159aac4d29ca7a9b5046518
Aidana Rezhabova <rezhabovaaidana.gmail.com>
2023-02-06 12:12:08 +0600

    math

--- code.py
+++ code.py
@@ -1,10 +1,12 @@
+import math
+
 #Hello world!
 print("Hello world!")
 def title():
     print("Title")
 def kvf(a):
-    return a*a;
+    return math.pow(a, 2);
 
 a=12
 res=kvf(a)
-print('a=', a)
+print('a=', res)
--- demo.java
+++ demo.java
@@ -2,11 +2,11 @@ public class Demo{
     public static void main(String[] args) {
         System.out.println("Helloooo")
         int a=12;
-        int res=kvf(a);
+        double res=kvf(a);
         System.out.print(res);
 
     }
     public static int kvf(int a){
-    return a*a
+    return Math.pow(a,2);
     }
 }
